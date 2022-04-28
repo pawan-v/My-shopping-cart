e9312f44b5ca4374ab40cceaf60c63a10c23038d
pawan-v <pawan1512001@gmail.com>
2022-04-28 16:08:46 +0530

    added increase decrease and delete button along with state'

--- hello/src/cartItem.js
+++ hello/src/cartItem.js
@@ -1,24 +1,51 @@
 import React from "react";
 
 class CartItem extends React.Component {
+
+    constructor (){
+        super() ;
+        this.state = {
+            price : 999,
+            title:'Phone',
+            qty : 1,
+            img: ''
+        }
+    }
+
+
   render() {
-      // eslint-disable-next-line jsx-a11y/alt-text
-      const newLocal_1 = <img style={styles.image} />;
-      const newLocal = newLocal_1;
+    // eslint-disable-next-line jsx-a11y/alt-text
+    const newLocal_1 = <img style={styles.image} />;
+    const {prices , title, qty} = this.state ;
+    const newLocal = newLocal_1;
+
+    
     return (
+
+
       <div className="cart-item">
-        <div className="left-block">
-          {newLocal}
-        </div>
+        <div className="left-block">{newLocal}</div>
 
         <div className="right-block">
-          <div style={{ fontSize: 25 }}>Phone</div>
-          <div style={{ color: "#777" }}>Rs 999</div>
-          <div style={{ color: "#777" }}>Qty : 1</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: "#777" }}>Rs {prices}</div>
+          <div style={{ color: "#777" }}>Qty : {qty}</div>
           <div className="cart-item-actions">
-              <img alt="increase" className="action-icons" src="" />
-              <img alt="decrease" className="action-icons" src="" />
-              <img alt="delete" className="action-icons" src="" />
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992651.png"
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+            />
           </div>
         </div>
       </div>
@@ -31,6 +58,7 @@ const styles = {
     height: 110,
     width: 110,
     borderRadius: 4,
+    backgroundColor:'#777'
   },
 };
 
